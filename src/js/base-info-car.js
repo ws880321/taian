a8871c82f637f23038e6c8b9a1098e2081787ef5
ws880321 <wangshuai@vip.126.com>
2016-12-06 16:56:15 +0800

    分页和左侧2

---
+++ src/js/base-info-car.js
@@ -0,0 +1,33 @@
+//引入公共模块
+var pb = require('./common/public.js');
+// 引入下拉菜单
+require('select2');
+require('pagination');
+
+
+
+
+
+
+
+
+// 左侧菜单效果
+pb.slide();
+// 美化checkbox 和全选
+pb.checkbox('.ck-input')
+
+// 初始化下拉
+$(".s-select").select2({
+    minimumResultsForSearch: -1,
+});
+
+
+        
+// 创建分页
+$("#page").pagination(10, {
+    num_edge_entries: 1, //边缘页数
+    num_display_entries: 4, //主体页数
+    // callback: ,
+    items_per_page: 1 //每页显示1项
+});
+
