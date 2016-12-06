5065c1faf9b7745c51c251b3373f7069c2cca461
ws880321 <wangshuai@vip.126.com>
2016-12-06 16:52:13 +0800

    分页和左侧

--- src/js/common/public.js
+++ src/js/common/public.js
@@ -1,9 +1,15 @@
 // 左侧菜单伸缩
-exports.slide=function(){
+exports.slide=function(currentArray){
 	$('#left-bar h3 a').click(function(){
 		$(this).toggleClass('active');
 		$(this).closest('h3').next('ul').slideToggle();
 	})
+	if (currentArray && currentArray instanceof Array) {
+		var _that=$('#left-bar h3').eq(currentArray[0])
+		_that.find('a').addClass('active')
+		_that.next('ul').slideToggle();
+		_that.next('ul').find('li').eq(currentArray[1]).find('a').addClass('active')
+	}
 }
 // 全选和美化
 exports.checkbox = function(obj) {
--- src/js/dispatchd.js
+++ src/js/dispatchd.js
@@ -4,6 +4,15 @@ var pb = require('./common/public.js');
 require('select2');
 // 引入日期
 require('laydate');
+require('pagination');
+
+
+
+
+
+
+
+
 // 左侧菜单效果
 pb.slide();
 // 美化checkbox 和全选
@@ -21,3 +30,14 @@ laydate({
 laydate({
     elem: '#end-date'
 });
+
+
+        
+// 创建分页
+$("#page").pagination(10, {
+    num_edge_entries: 1, //边缘页数
+    num_display_entries: 4, //主体页数
+    // callback: ,
+    items_per_page: 1 //每页显示1项
+});
+
